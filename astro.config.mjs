4e6c17bf938fc320e7c0ba608af1746cbd6a1767
angelcorral2021 <angelvolta@hotmail.com>
2024-09-19 14:42:16 -0300

    Proyecto base2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://angelcorral2021.github.io',
+    base: 'port-blog',
     integrations: [
         mdx(),
         sitemap(),
