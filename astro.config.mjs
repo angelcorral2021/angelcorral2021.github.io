6acd7335c13ee50e4716b67e0e582aeb34d5763d
Angel Corral <angelvolta@hotmail.com>
2025-10-26 18:36:02 -0300

    build: deploy to main2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://angelcorral2021.github.io',
+  site: 'https://angelcorral2021.github.io/angelcorral2021.github.io',
   base: '/',
   integrations: [tailwind()],
 });
