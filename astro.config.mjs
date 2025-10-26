6bccfc11639513adefabcac68fd24488e5228e9e
Angel Corral <angelvolta@hotmail.com>
2025-10-26 17:59:28 -0300

    Nuevo archivo 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-export default defineConfig({
+/*export default defineConfig({
   integrations: [tailwind()],
   base: "/angelcorral2021/", // 👈 correcto para angelcorral2021.github.io
   
+});
+*/
+
+export default defineConfig({
+  site: 'https://angelcorral2021.github.io', // 👈 muy importante
+  base: '/', // 👈 raíz del sitio
 });
